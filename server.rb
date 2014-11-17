3c4ab323c884010ac055778885f01d4e5723773b
1278dale <dalestevens1278@gmail.com>
2014-11-17 18:20:02 +0000

    Updated adding links spec and server.rb

--- server.rb
+++ server.rb
@@ -1,16 +1,28 @@
+require 'data_mapper'
+require 'sinatra'
+
+set :views, Proc.new{File.join(root, 'views')}
+
 env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialised
-require 'data_mapper'
+
 # After declaring your models, you should finalise them
 DataMapper.finalize
-
+  
 # However, the database tables don't exist yet. Let's tell datamapper to create them
 DataMapper.auto_upgrade!
 
+get '/' do
+  @links = Link.all
+  erb :index
+end
 
-# So, we begin by telling datamapper where our database is going to be. The second argument to setup() is called a connection string. It has the following format.
-
-# dbtype://user:password@hostname:port/databasename
+post '/links' do
+  url = params["url"]
+  title = params["title"]
+  Link.create(:url => url, :title => title)
+  redirect to('/')
+end
