378c992925c6b8cf56e25a974a5f9bdf1169c47f
1278dale <dalestevens1278@gmail.com>
2014-11-20 17:35:41 +0000

    added unique attribute to user model

--- lib/user.rb
+++ lib/user.rb
@@ -6,7 +6,9 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, :unique => true
+
+  
   # this will store both the password and the salt
   # It's Text and not String because String holds
   # 50 characters by default
@@ -20,7 +22,7 @@ class User
 # and password_confirmation are the same
 # read more about it in the documentation
 # http://datamapper.org/docs/validations.html
-  validates_confirmation_of :password
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
 
 # ((((The reason we need the reader for :password and :password_confirmation is that datamapper should have access to both values to make sure they are the same.
 
